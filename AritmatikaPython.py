368ebe3bb7f92bd4ae06d3b31a9f59425c6fdf97
unknown <erlinsetyowati154@gmail.com>
2021-11-24 15:27:33 +0700

    commit pertama

---
+++ AritmatikaPython.py
@@ -0,0 +1,36 @@
+# Operasi Aritmatika dalam Python
+
+a = 10
+b = 3
+
+# Operasi Pertambahan (+)
+hasil = a+b
+print(a,'+',b,'=',hasil)
+
+# Operasi Pengurangan (-)
+hasil = a-b
+print(a,'-',b,'=',hasil)
+
+# Operasi Peekalian (*)
+hasil = a*b
+print(a,'*',b,'=',hasil)
+
+# Operasi Pemabagian (/)
+hasil = a/b
+print(a,'/',b,'=',hasil)
+
+# Operasi Eksponen / Pangkat (**)
+hasil = a**b
+print(a,'**',b,'=',hasil)
+
+# Operasi Modulus / Hasil dari sisa pembagian (%)
+hasil = a%b
+print(a,'%',b,'=',hasil)
+
+# Operasi Floor Division / Pembulatan hasil dari pembagian (//)
+hasil = a//b
+print(a,'//',b,'=',hasil)
+
+# Prioritas Operasi Aritmatika
+hasil = a+b*a/b
+print(a,'+',b,'*',a,'/',b,'=',hasil)
